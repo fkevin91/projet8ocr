8a97849c9a1d802a496127a416db13e9cdbeec2e
kevin fardeau <k.fardeau123@gmail.com>
2022-03-17 18:51:23 +0000

    Add Fixtures fixed #7

---
+++ projet8-TodoList/src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i=0; $i < 20; $i++) {
+            $task = (new Task())
+                ->setCreatedAt(new \DateTime())
+                ->setTitle('Task'.$i)
+                ->setContent('contenu'.$i)
+                ->setIsDone(0);
+            $manager->persist($task);
+        }
+        $manager->flush();
+    }
+}
---
+++ projet8-TodoList/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i=0; $i < 20; $i++) { 
+            $user = (new User())
+                ->setUsername('usertest'.$i)
+                ->setPassword('azerty')
+                ->setEmail('aze'.$i.'@gmail.com');
+            $manager->persist($user);
+        }
+        // ajouter un hachage au mot de passe
+
+        $manager->flush();
+    }
+}
