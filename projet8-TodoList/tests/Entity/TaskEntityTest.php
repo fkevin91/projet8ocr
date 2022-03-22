5eb6c85bca4534c7cd96baf1a48feef458b718d5
kevin fardeau <k.fardeau123@gmail.com>
2022-03-22 18:59:34 +0000

    add test entity

--- projet8-TodoList/tests/Entity/TaskEntityTest.php
+++ projet8-TodoList/tests/Entity/TaskEntityTest.php
@@ -1,17 +1,58 @@
 <?php
 
-namespace App\Tests;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+namespace App\Tests\Entity;
 
-class TaskEntityTest extends KernelTestCase
+
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TaskTest extends WebTestCase
 {
-    public function testSomething(): void
-    {
-        $kernel = self::bootKernel();
+    private $task;
 
-        $this->assertSame('test', $kernel->getEnvironment());
-        //$routerService = static::getContainer()->get('router');
-        //$myCustomService = static::getContainer()->get(CustomService::class);
+    private $date;
+
+    public function setUp(): void
+    {
+        $this->task = new Task();
+        $this->date = new \DateTime();
     }
+
+    public function testCreatedAt(): void
+    {
+        $this->task->setCreatedAt($this->date);
+        $this->assertSame($this->date, $this->task->getCreatedAt());
+    }
+
+    public function testId(): void
+    {
+        $this->assertNull($this->task->getId());
+    }
+
+    public function testTitle(): void
+    {
+        $this->task->setTitle('Test du titre');
+        $this->assertSame($this->task->getTitle(), 'Test du titre');
+    }
+
+    public function testContent(): void
+    {
+        $this->task->setContent('Test du contenu');
+        $this->assertSame($this->task->getContent(), 'Test du contenu');
+    }
+
+    public function testIsDone(): void
+    {
+        $this->task->setIsDone(true);
+        $this->assertEquals($this->task->getIsDone(), true);
+    }
+
+    public function testUser(): void
+    {
+        $this->task->setUser(new User());
+        $this->assertInstanceOf(User::class, $this->task->getUser());
+    }
+
 }
--- projet8-TodoList/tests/Entity/UserEntityTest.php
+++ projet8-TodoList/tests/Entity/UserEntityTest.php
@@ -1,17 +1,74 @@
 <?php
 
-namespace App\Tests;
+namespace App\Tests\Entity;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Entity\Task;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class UserEntityTest extends KernelTestCase
+class UserTest extends WebTestCase
 {
-    public function testSomething(): void
-    {
-        $kernel = self::bootKernel();
+    private $user;
 
-        $this->assertSame('test', $kernel->getEnvironment());
-        //$routerService = static::getContainer()->get('router');
-        //$myCustomService = static::getContainer()->get(CustomService::class);
+    private $task;
+
+    public function setUp(): void
+    {
+        $this->user = new User();
+        $this->task = new Task();
     }
+
+    public function testId(): void
+    {
+        $this->user->setId(1);
+        $this->assertSame(1, $this->user->getId());
+    }
+
+    public function testUsername(): void
+    {
+        $this->user->setUsername('kevin');
+        $this->assertSame('kevin', $this->user->getUsername());
+    }
+
+    public function testPassword(): void
+    {
+        $this->user->setPassword('motdepassetest');
+        $this->assertSame('motdepassetest', $this->user->getPassword());
+    }
+
+    public function testEmail(): void
+    {
+        $this->user->setEmail('k.fardeau@gmail.com');
+        $this->assertSame('k.fardeau@gmail.com', $this->user->getEmail());
+    }
+
+    public function testRoles(): void
+    {
+        $this->user->setRoles(['ROLE_USER']);
+        $this->assertSame(['ROLE_USER'], $this->user->getRoles());
+    }
+
+    public function testTasks(): void
+    {
+        $tasks = $this->user->getTask($this->task->getUser());
+        $this->assertSame($this->user->getTask(), $tasks);
+
+        $this->user->addtask($this->task);
+        $this->assertCount(1, $this->user->getTask());
+
+        $this->user->removeTask($this->task);
+        $this->assertCount(0, $this->user->getTask());
+    }
+
+
+    public function testSalt(): void
+    {
+        $this->assertEquals(null, $this->user->getSalt());
+    }
+
+    public function testEraseCredential(): void
+    {
+        $this->assertNull($this->user->eraseCredentials());
+    }
+
 }
